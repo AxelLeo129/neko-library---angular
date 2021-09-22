45a2773b13136214246050d1a5f08fd4c990c599
AxelLeo129 <axelleo129dev@gmail.com>
2021-09-22 00:50:53 -0600

    Dynamic slider.

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { faChevronRight, faAngleUp, faAngleDown } from '@fortawesome/free-solid-svg-icons';
 import { toggle } from 'slidetoggle';
+import { GeneralService } from 'src/app/services/general.service';
 
 @Component({
   selector: 'app-slider',
@@ -10,21 +12,28 @@ import { toggle } from 'slidetoggle';
 })
 export class SliderComponent implements OnInit {
 
-  public imagen_fondo: string = "https://firebasestorage.googleapis.com/v0/b/neko-library.appspot.com/o/slide%2Fback_default.jpg?alt=media&token=d12ba168-bcad-40f6-b51d-71dbac93fd48";
-  public imagen_fondo1: string = "https://firebasestorage.googleapis.com/v0/b/neko-library.appspot.com/o/slide%2Ffondo2.jpg?alt=media&token=a03912a3-d37c-4106-8eda-833e69ce9bc9";
-  public imagen_fondo2: string = "https://firebasestorage.googleapis.com/v0/b/neko-library.appspot.com/o/slide%2Ffondo3.jpg?alt=media&token=15b1a7ab-466a-4f6a-ac57-4a0babeb63e1";
   public faChevronRight = faChevronRight;
   public faAngle = faAngleUp;
+  public cursore: Array<any>[] = [];
 
-  images = [
-    "https://firebasestorage.googleapis.com/v0/b/neko-library.appspot.com/o/slide%2Fcalzado.png?alt=media&token=9b5fa55f-a4f7-4140-9a1e-4018600a51e4",
-    "https://firebasestorage.googleapis.com/v0/b/neko-library.appspot.com/o/slide%2Fcurso.png?alt=media&token=106af9e6-fcee-4b1e-832d-856ac449fa3f",
-    "https://firebasestorage.googleapis.com/v0/b/neko-library.appspot.com/o/slide%2Fiphone.png?alt=media&token=2c992069-ac3b-417e-91b9-e0261af13161"
-  ];
-
-  constructor() { }
+  constructor(private generalService: GeneralService, private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
+    this.getSlider();
+  }
+
+  getSlider() {
+    this.generalService.get('slider').then((response) => {
+      response.forEach(element => {
+        element.stile_immagine = JSON.parse(element.stile_immagine);
+        element.stile_testo = JSON.parse(element.stile_testo);
+        element.titolo1 = JSON.parse(element.titolo1);
+        element.titolo2 = JSON.parse(element.titolo2);
+        element.titolo3 = JSON.parse(element.titolo3);
+        element.pulsante = this.sanitizer.bypassSecurityTrustHtml(element.pulsante);
+        this.cursore.push(element);
+      });
+    }).catch(err => console.log(err));
   }
 
   hideSlide(): void {
